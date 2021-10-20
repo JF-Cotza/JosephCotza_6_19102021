6d82e2fb8bf82de9c063e35c259d6fd13fa46ab8
Joseph Cotza <joseph.cotza.01@gmail.com>
2021-10-20 14:54:13 +0200

    ajout du répertoire image dans le backend et modification de .gitignore

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator'); //on a besoin du module mongoose-unique-validator
+const uniqueValidator = require('mongoose-unique-validator');   //on a besoin du module mongoose-unique-validator
 
 const userSchema = mongoose.Schema({                            //on crée le modèle
-    email: { type: String, required: true, unique: true },        //unique => empêche en théorie le multi enregistrement avec le même mail
+    email: { type: String, required: true, unique: true },      //unique => empêche en théorie le multi enregistrement avec le même mail
     password: { type: String, required: true }
 });
 
-userSchema.plugin(uniqueValidator);                                 //uniqueValidator renforce la sécurité et évite des bugs
+userSchema.plugin(uniqueValidator);                             //uniqueValidator renforce la sécurité et évite des bugs
 
 module.exports = mongoose.model('User', userSchema);            //on l'exporte
 
