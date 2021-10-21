08b4dabff33723c3054560a371a525ec8b8e3037
Joseph Cotza <joseph.cotza.01@gmail.com>
2021-10-22 01:41:54 +0200

    création d une route et d un controleur pour la connexion à la db et ne pas importer identifiant et MDP dans github

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,6 @@
 //ajout des modules
 const express=require('express');
-const mongoose=require('mongoose');
 
-const bcrypt = require('bcrypt');          
-const jwt = require('jsonwebtoken');
-const multer = require('multer');
-const path = require('path');
 
 //création de app, "l'application du backend"
 const app=express();
@@ -14,26 +9,22 @@ const app=express();
 const userRoutes=require('./routes/user');
 const sauceRoutes=require('./routes/sauce');
 
-const connect=require('./variable/ext-dat');
-
-//test
-//pour éviter les CORS
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
+const connect=require('./routes/connect');
 
 app.use(express.json());
 
 app.use('/api/sauce', sauceRoutes); // on appelle stuffRoutes comme 'fonction' de app. 
 app.use('/api/auth', userRoutes);
-/*
-app.use('/images', express.static(path.join(__dirname, 'images')));  //le répertoire est fixe => on utilise static pour pouvoir l'utilser 
-*/
 
 /*générique : export des datas*/
 module.exports = app;
 
 
+/* élément vus en cours non utilisés :
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const multer = require('multer');
+const path = require('path');
+app.use('/images', express.static(path.join(__dirname, 'images')));  //le répertoire est fixe => on utilise static pour pouvoir l'utilser
+
+*/
--- backend/routes/connect.js
+++ backend/routes/connect.js
@@ -1,15 +1,9 @@
 
 const mongoose = require('mongoose');
+const link=require('../controllers/connect');
 
 //connexion à la DB
-const userDb = 'P1qu4nt3_MNG';
-const pswordDb = 'M4n4g1nGC0D3F0RTh1s0n3';
-const linkDb = 'cluster0.yepbw.mongodb.net';
-const nameDb = 'piiq_db';
-const link = 'mongodb+srv://' + userDb + ':' + pswordDb + '@' + linkDb + '/' + nameDb + '?retryWrites=true&w=majority';
-
-//connexion à la DB
-mongoose.connect(link,
+mongoose.connect(JSON.stringify(link.link),
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -18,7 +12,6 @@ mongoose.connect(link,
         console.log('Connexion à MongoDB échouée !'),
             console.log(error.message)
     });
-
 
 module.export=mongoose.connect;
 
